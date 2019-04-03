99aa57da41ff89612caa4fcca1f6421878ce24a1
Alex <therebeloper@gmail.com>
2019-04-03 12:02:08 +0300

    Initial Commit

---
+++ UISwitchProgrammaticallySwift/RootViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RootViewController.swift
+//  UISwitchProgrammaticallySwift
+//
+//  Created by Alex Nagy on 03/04/2019.
+//  Copyright © 2019 Alex Nagy. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
