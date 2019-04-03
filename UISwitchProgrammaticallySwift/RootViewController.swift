3e9a85d0a0ce2f8ea504c56482e5430628945af1
Alex <therebeloper@gmail.com>
2019-04-03 12:16:55 +0300

    first commit

--- UISwitchProgrammaticallySwift/RootViewController.swift
+++ UISwitchProgrammaticallySwift/RootViewController.swift
@@ -6,13 +6,39 @@
 //  Copyright © 2019 Alex Nagy. All rights reserved.
 //
 
-import UIKit
+import TinyConstraints
 
 class RootViewController: UIViewController {
+    
+    lazy var themeSwitch: UISwitch = {
+        let themeSwitch = UISwitch()
+        themeSwitch.tintColor = #colorLiteral(red: 0.9411764706, green: 0.1882352941, blue: 0.1882352941, alpha: 1)
+        themeSwitch.onTintColor = #colorLiteral(red: 0.9411764706, green: 0.1882352941, blue: 0.1882352941, alpha: 1)
+        themeSwitch.thumbTintColor = #colorLiteral(red: 0.1529411765, green: 0.1529411765, blue: 0.1529411765, alpha: 1)
+        themeSwitch.backgroundColor = #colorLiteral(red: 0.968627451, green: 0.968627451, blue: 0.968627451, alpha: 1)
+        themeSwitch.layer.cornerRadius = 16
+        themeSwitch.layer.masksToBounds = true
+        themeSwitch.addTarget(self, action: #selector(didChangeThemeSwitchValue), for: .valueChanged)
+        return themeSwitch
+    }()
+    
+    @objc func didChangeThemeSwitchValue() {
+        if themeSwitch.isOn {
+            print("Switch is on")
+        } else {
+            print("Switch is off")
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        view.backgroundColor = #colorLiteral(red: 0.968627451, green: 0.968627451, blue: 0.968627451, alpha: 1)
+        
+        view.addSubview(themeSwitch)
+        themeSwitch.centerInSuperview()
+        
     }
 
 
